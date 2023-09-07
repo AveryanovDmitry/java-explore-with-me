b1b8ddc320a9afe530fa22da88d8ac35b8bba74e
Dmitry Averyanov <averyanov-22@yandex.ru>
2023-09-07 19:41:37 +0300

    добавлены изменения после проверки

--- statistic/statistic-dto/src/main/java/ru/practicum/dto/RequestHitDto.java
+++ statistic/statistic-dto/src/main/java/ru/practicum/dto/RequestHitDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
@@ -11,15 +12,16 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 public class RequestHitDto {
-    @NotNull(message = "field app can't be null")
+    @NotBlank(message = "field app can't be null or empty")
     @Size(max = 255)
     private String app;
-    @NotNull(message = "field uri can't be null")
+    @NotBlank(message = "field uri can't be null or empty")
     @Size(max = 255)
     private String uri;
-    @NotNull(message = "field ip can't be null")
+    @NotBlank(message = "field ip can't be null or empty")
     @Size(max = 255)
     private String ip;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @NotNull(message = "field timestamp can't be null")
     private LocalDateTime timestamp;
 }
